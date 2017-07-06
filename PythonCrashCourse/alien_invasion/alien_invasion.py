eea093dfb11fb2511054658bc1875e51a12bb297
joyc <joyc@live.jp>
2017-07-07 00:51:59 +0900

    init

--- PythonCrashCourse/alien_invasion/alien_invasion.py
+++ PythonCrashCourse/alien_invasion/alien_invasion.py
@@ -27,8 +27,9 @@ def run_game():
     # 创建Play按钮
     play_button = Button(ai_settings, screen, "Play")
 
-    # Create an instance to store game statistics.
+    # 创建存储游戏统计信息的实例， 并创建记分牌
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # 设置背景颜色
     bg_color = (230, 230, 230)
@@ -40,13 +41,6 @@ def run_game():
 
     # 创建外星人群
     gf.create_fleet(ai_settings, screen, ship, aliens)
-
-    # 创建一个用于存储游戏统计信息的实例
-    stats = GameStats(ai_settings)
-
-    # 创建存储游戏统计信息的实例， 并创建记分牌
-    stats = GameStats(ai_settings)
-    sb = Scoreboard(ai_settings, screen, stats)
 
     # 开始游戏主循环
     while True:
--- PythonCrashCourse/alien_invasion/bullet.py
+++ PythonCrashCourse/alien_invasion/bullet.py
@@ -22,6 +22,7 @@ class Bullet(Sprite):
 
         # 存储用小数表示的子弹位置
         self.y = float(self.rect.y)
+
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
 
