3020a65101cb57bbf0c1b67a7548a0d12ee45b7e
joyc <joyc@live.jp>
2018-02-05 00:25:01 +0900

    add files

--- corepython2/Ch11/oddnogen.py
+++ corepython2/Ch11/oddnogen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from random import randint
+# from random import randint
 
 
 # def odd(n):
@@ -19,7 +19,11 @@ from random import randint
 
 
 # 2--------
-allNums = []
-for eachNum in range(9):
-    allNums.append(randint(1, 99))
-print [n for n in allNums if n % 2]
+# allNums = []
+# for eachNum in range(9):
+#     allNums.append(randint(1, 99))
+# print [n for n in allNums if n % 2]
+
+# 3--------
+from random import randint as ri
+print [n for n in [ri(1,99) for i in range(9)] if n % 2]
