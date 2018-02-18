a7ddc9a2e4d24b26f908cd70124c2e1d9ba78dbd
joyc <joyc@live.jp>
2018-02-18 17:58:53 +0900

    add files

--- corepython2/Ch15/retest.py
+++ corepython2/Ch15/retest.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-<<<<<<< HEAD
 import re
 
 # m = re.match('foo', 'seafood')      # 使用match() 查不到
@@ -10,11 +9,3 @@ import re
 n = re.search('foo', 'seafood')     # 改用search()
 if n is not None: n.group()
 print n.group()
-=======
-
-
-import re
-m = re.match('foo', 'foo')
-if m is not None:
-    print m.group()
->>>>>>> origin/master
