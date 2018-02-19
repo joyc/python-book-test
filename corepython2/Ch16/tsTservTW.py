02ecc0c7d8763106c364b86da161863951a17021
chuang.li <chuang.li.eb@bp.renesas.com>
2018-02-19 16:05:17 +0900

    add new files of socket:Twisted

--- corepython2/Ch16/tsTservTW.py
+++ corepython2/Ch16/tsTservTW.py
@@ -8,12 +8,15 @@ from time import ctime
 
 PORT = 21567
 
+
 class TSServProtocol(protocol.Protocol):
     def connectionMade(self):
         clnt = self.clnt = self.transport.getPeer().host
         print '...connected from:', clnt
+
     def dataReceived(self, data):
         self.transport.write('[%s] %s' % (ctime(), data))
+
 
 factory = protocol.Factory()
 factory.protocol = TSServProtocol
