22e1f461766db3e0ea9e6e55a5533db385d0113d
joyc <joyc@live.jp>
2018-05-20 01:08:27 +0900

    init new book

---
+++ flask_test/search_page/index.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+
+from flask import Flask, request, render_template
+from spiderData import search_info
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('/search.html')
+    return render_template('/result.html', name='lisi',)
+
+
+@app.route('/search')
+def search():
+    keyword = request.args.get('wd')
+    print(keyword)
+    result = search_info(keyword)
+    return render_template('/result.html', data=result, num=len(result))
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>hello, %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    app.run()
