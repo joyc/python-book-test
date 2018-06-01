ea7d8461675840f42263590e95be38d62f2bd70f
joyc <joyc@live.jp>
2018-06-02 00:39:36 +0900

    add new file

---
+++ Beginning_Python_3E/ch16/my_math.py
@@ -0,0 +1,23 @@
+def square(x):
+    '''
+    计算平方并返回结果
+    >>> square(5)
+    25
+    >>> square(8)
+    64
+    '''
+    return x * x
+
+
+def product(x, y):
+    '''
+    计算乘积并返回结果
+    :param x:
+    3
+    :param y:
+    5
+    :return:
+    15
+    '''
+    pass
+    #return x * y
