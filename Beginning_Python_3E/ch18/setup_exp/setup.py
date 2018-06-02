14481a0a8a8f0f4f6a618f73590ffdc6896efdf6
joyc <joyc@live.jp>
2018-06-03 00:36:40 +0900

    add ch19 file

---
+++ Beginning_Python_3E/ch18/setup_exp/setup.py
@@ -0,0 +1,9 @@
+# 简单的Setuptools安装脚本（setup.py）
+from setuptools import setup
+
+
+setup(name='Hello',
+    version='1.0',
+    description='A simple example',
+    author='Hython.com',
+    py_modules=['hello'])
