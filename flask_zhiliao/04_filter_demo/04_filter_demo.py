67d49d11326c5a7952b673896d447500e0e99add
joyc <joyc@live.jp>
2018-06-25 01:14:48 +0900

    add jinjia2 filter demo

---
+++ flask_zhiliao/04_filter_demo/04_filter_demo.py
@@ -0,0 +1,14 @@
+#! python3
+# -*- coding:utf-8 -*-
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', position=-9)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
