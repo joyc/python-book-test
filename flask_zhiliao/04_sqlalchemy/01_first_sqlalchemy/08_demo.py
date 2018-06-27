4b1b832b2f6c5d8deb8043b65cc6d90baafc559c
Li <chuang.li@accenture.com>
2018-06-27 10:51:18 +0900

    add

--- flask_zhiliao/04_sqlalchemy/01_first_sqlalchemy/08_demo.py
+++ flask_zhiliao/04_sqlalchemy/01_first_sqlalchemy/08_demo.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine, Column, Integer, Float, String, \
-    func, and_, or_
+from sqlalchemy import create_engine, Column, Integer, Float, String, Text, \
+    func, and_, or_, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import random
 
 
-HOSTNAME = '10.211.55.4'
+HOSTNAME = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'first_sqlalchemy'
 USERNAME = 'root'
@@ -22,8 +22,27 @@ session = sessionmaker(engine)()
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column()
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), nullable=False)
 
 
 class Article(Base):
-    pass
+    __tablename__ = 'article'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(50), nullable=False)
+    content = Column(Text, nullable=False)
+    # 默认约束就是ondelete="RESTRICT"，不用添加。
+    uid = Column(Integer, ForeignKey("user.id", ondelete="RESTRICT"))
+
+
+Base.metadata.drop_all()
+Base.metadata.create_all()
+
+user = User(username='zhiliao')
+session.add(user)
+session.commit()
+
+article = Article(title='abcs',content='12345',uid=1)
+session.add(article)
+session.commit()
+
