839696fd7215e0cd5949f6552b40ac38583bc7cd
joyc <joyc@live.jp>
2018-07-05 00:13:44 +0900

    init zlbbs common config files

--- flask_zhiliao/zlbbs/config.py
+++ flask_zhiliao/zlbbs/config.py
@@ -1,3 +1,14 @@
 #encoding: utf-8
 
 DEBUG = True
+
+DB_USERNAME = 'root'
+DB_PASSWORD = 'root'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_NAME = 'zlbbs'
+
+DB_URI = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8"
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- flask_zhiliao/zlbbs/exts.py
+++ flask_zhiliao/zlbbs/exts.py
@@ -1,1 +1,6 @@
-pass
+#encoding: utf-8
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
