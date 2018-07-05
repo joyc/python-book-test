50180491b30c94eb06708220cb181db01dd14d4e
joyc <joyc@live.jp>
2018-07-06 02:06:04 +0900

    add cms template

--- flask_zhiliao/zlbbs/apps/cms/decorators.py
+++ flask_zhiliao/zlbbs/apps/cms/decorators.py
@@ -8,7 +8,7 @@ def login_required(func):
 
     @wraps(func)
     def inner(*args, **kwargs):
-        if CMS_USER_ID in session:
+        if config.CMS_USER_ID in session:
             return func(*args, **kwargs)
         else:
             return redirect(url_for('cms.login'))
