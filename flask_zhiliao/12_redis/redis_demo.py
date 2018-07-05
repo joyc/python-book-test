50fc731cf74696c18834f58d0769868eca4ed8f6
Li <chuang.li@accenture.com>
2018-07-05 18:01:43 +0900

    add template

--- flask_zhiliao/12_redis/redis_demo.py
+++ flask_zhiliao/12_redis/redis_demo.py
@@ -43,7 +43,3 @@ while True:
             data = item['data']
             print(data)
             # 发送邮件
-<<<<<<< HEAD
-
-=======
->>>>>>> 6214d731a0476131476c4c721d0e6bd0f969e424
--- flask_zhiliao/zlbbs/apps/cms/views.py
+++ flask_zhiliao/zlbbs/apps/cms/views.py
@@ -1,10 +1,48 @@
-#encoding: utf-8
+# encoding: utf-8
 
-from flask import Blueprint
+from flask import Blueprint, views, render_template, request, session, \
+    redirect, url_for
+from .forms import LoginForm
+from .models import CMSUser
+from .decorators import login_required
+import config
+
 
 bp = Blueprint("cms", __name__, url_prefix="/cms")
 
 
 @bp.route('/')
+@login_required
 def index():
     return 'cms index'
+
+
+class LoginView(views.MethodView):
+
+    def get(self, message=None):
+        return render_template("cms/cms_login.html", message=message)
+
+    def post(self):
+        form = LoginForm(request.form)
+        if form.validate():
+            email = form.email.data
+            password = form.password.data
+            remember = form.remember.data
+            user = CMSUser.query.filter_by(email=email).first()
+            if user and user.check_password(password):
+                session[config.CMS_USER_ID] = user.id
+                if remember:
+                    # 如果设置permanent = True则过期时间为31天或者从config中自定义设置
+                    session.permanent = True
+                    # 即便是当前蓝图也需要指定
+                return redirect(url_for("cms.index"))
+            else:
+                return self.get(message='邮箱或密码错误')
+        else:
+            # print(form.errors)
+            message = form.errors.popitem()[1][0]
+            # {'password': ['请输入正确格式的密码']}
+            return self.get(message='表单验证错误')
+
+
+bp.add_url_rule('/login/', view_func=LoginView.as_view('login'))
