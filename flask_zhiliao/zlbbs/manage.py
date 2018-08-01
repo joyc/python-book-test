bc1e6ca926c67d1bf036baed96289c576d339444
joyc <joyc@live.jp>
2018-08-02 00:37:26 +0900

    add front user model

--- flask_zhiliao/zlbbs/manage.py
+++ flask_zhiliao/zlbbs/manage.py
@@ -5,11 +5,14 @@ from flask_migrate import Migrate, MigrateCommand
 from zlbbs import create_app
 from exts import db
 from apps.cms import models as cms_models
+from apps.front import models as front_models
 
 
 CMSUser = cms_models.CMSUser
 CMSRole = cms_models.CMSRole
 CMSPermission = cms_models.CMSPermission
+
+FrontUser = front_models.FrontUser
 
 app = create_app()
 
@@ -67,6 +70,15 @@ def add_user_to_role(email, name):
             print("没有这个角色：%s" % role)
     else:
         print("邮箱%s没有这个用户。" %email)
+
+
+@manager.option('-t', '--telephone', dest='telephone')
+@manager.option('-u', '--username', dest='username')
+@manager.option('-p', '--password', dest='password')
+def create_front_user(telephone, username, password):
+    user = FrontUser(telephone=telephone, username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
 
 
 @manager.command
