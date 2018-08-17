989c072e7ee7a573c1eb160f31f1cbde324cbb97
joyc <joyc@live.jp>
2018-08-17 23:56:43 +0900

    add GUI program

--- GUI/Ch01/GUI_combobox_widget.py
+++ GUI/Ch01/GUI_combobox_widget.py
@@ -14,9 +14,13 @@ win.title("Python GUI")
 a_label = ttk.Label(win, text="A Label")
 a_label.grid(column=0, row=0)
 
+
 # Modified Button Click Function
 def click_me():
-    action.configure(text='Hello ' + name.get())
+    action.configure(text='Hello ' + number_chosen.get() + ' ' + name.get())
+#     print(number)
+#     print(number.get())
+
 
 # Changing our Label
 ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
@@ -24,12 +28,18 @@ ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
 # Adding a Textbox Entry widget
 name = tk.StringVar()
 name_entered = ttk.Entry(win, width=12, textvariable=name)
-name_entered.grid(column=0, row=1)
+name_entered.grid(column=0, row=1)                          # column 0
 
 # Adding a Button
 action = ttk.Button(win, text="Click Me!", command=click_me)
-action.grid(column=1, row=1)
-# action.configure(state='disabled')      # Disable the Button Widget
+action.grid(column=2, row=1)                                # <= change column to 2
+
+ttk.Label(win, text="Choose a number:").grid(column=1, row=0)
+number = tk.StringVar()
+number_chosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+number_chosen['values'] = (1, 2, 4, 42, 100)
+number_chosen.grid(column=1, row=1)                         # <= Combobox in column 1
+number_chosen.current(0)
 
 name_entered.focus()      # Place cursor into name Entry
 #======================
