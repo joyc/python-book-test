81541abcf01bbfba027f67497de9b93d6ed1a06f
joyc <joyc@live.jp>
2018-09-03 23:03:30 +0900

    add test code

---
+++ PythonTutorial/ch07/fstring.py
@@ -0,0 +1,22 @@
+import math
+
+print(f'The value of pi is approximately {math.pi:.3f}.')
+print('The value of pi is approximately %5.3f.' % math.pi)
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+for name, phone in table.items():
+    print(f'{name:10} ==> {phone:10d}')
+
+print('The story of {0}, {1}, and {other}.'.format('Bill', 'Manfred', other='Georg'))
+
+print('Jack: {0[Jack]:d}; Sjoerd: {0[Sjoerd]:d}; '
+      'Dcab: {0[Dcab]:d}'.format(table))
+
+print('Jack: {Jack:d}; Sjoerd: {Sjoerd:d}; Dcab: {Dcab:d}'.format(**table))
+
+
+for x in range(1, 11):
+    print('{0:2d} {1:3d} {2:4d}'.format(x, x*x, x*x*x))
+
+for x in range(1, 11):
+    print(repr(x).rjust(2), repr(x * x).rjust(3), end=' ')
+    print(repr(x * x * x).rjust(4))
